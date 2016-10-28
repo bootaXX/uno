a2a6f053bd8464ed8b6a2ba7daeac4640b43cc39
Pauline Sarana <p_louie_s@yahoo.com>
2016-10-29 00:41:39 +0800

    Accomplished initGame problem. Week 2 done.

--- src/Game.hs
+++ src/Game.hs
@@ -1,13 +1,20 @@
 module Game where
 
 import Common
+import System.Console.ANSI (Color(Black, Red, Green, Yellow, Blue, Magenta, Cyan, White))
 
 initGame :: Int -> State
 
 -- TODO: Implement a method to initialize a new game given n players
-initGame n = State { players = [ ],
-                     deck = fullDeck,
+initGame n = State { players = generateHumanPlayers n,
+                     deck = Common.fullDeck,
                      d_stack = [ ] }
+-- MY CODES----------------------------------------------------------
+generateHumanPlayers :: Int -> [Player]
+generateHumanPlayers n 
+		| (n > 0) = [HPlayer {name = "Player" ++ show n, hand = [Card {color = Black, value = One}]}] ++ generateHumanPlayers (n-1)
+		| otherwise = [ ]
+---------------------------------------------------------------------
 
 -- TODO: Implement a method to setup the game
 setupGame :: State -> IO State
